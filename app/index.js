714ec56e40358775d8fba0699e10091469105eaa
leo <nascimentoleo899@gmail.com>
2024-02-15 17:01:43 -0300

    feat: implement http server api

---
+++ app/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const Blockchain = require('../blockchain');
+
+const app = express();
+app.use(express.json());
+const PORT = process.env.HTTP_PORT || 3000;
+
+const bc = new Blockchain();
+
+app.get('/blocks', (req, res) => {
+   res.json(bc.chain);
+});
+
+app.post('/mine', (req, res) => {
+   const block = bc.addBlock(req.body.data);
+   console.log(`New block added: ${block.toString()}`);
+   res.redirect('/blocks');
+});
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}...`));
